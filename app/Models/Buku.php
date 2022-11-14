85b361cd2b32cdfdf3ae96d46eec9811c590a8e8
nfal001 <111720828+nfal001@users.noreply.github.com>
2022-11-14 10:11:10 +0700

    First Commit
    First Commit

---
+++ app/Models/Buku.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Buku extends Model
+{
+    use HasFactory;
+    protected $fillable = ['title', 'author', 'description', 'image', 'user_id'];
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'id', 'user_id');
+    }
+}
---
+++ resources/views/buku/index.blade.php
@@ -0,0 +1,35 @@
+@extends('layouts.master')
+
+@section('content')
+<header class="container d-flex justify-content-between align-items-center mb-5">
+    <p class="display-4 text-center my-auto">List MyKomik</p>
+    <a href="" class="btn btn-primary">Input Komik Baru</a>
+</header>
+<table class="table">
+    <thead>
+        <tr>
+            <th scope="col">#</th>
+            <th scope="col">Image</th>
+            <th scope="col">Title</th>
+            <th scope="col">Author</th>
+            <th scope="col">Aksi</th>
+        </tr>
+    </thead>
+    <tbody>
+        @foreach($buku as $b)
+        <tr>
+            <th scope="row">{{$b->id}}</th>
+            <td>{{$b->image}}</td>
+            <td>{{$b->title}}</td>
+            <td>{{$b->author}}</td>
+            <td>
+                <form action="">
+                    <a class="btn btn-success" href="">Show</a>
+                    <input class="btn btn-danger" type="submit" value="Submit">
+                </form>
+            </td>
+        </tr>
+        @endforeach
+    </tbody>
+</table>
+@endsection
